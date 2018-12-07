256dc48c3c1c4390dc6eed835830f39bac6e2f62
Juan Diego <jumarinr@unal.edu.co>
2018-12-06 23:11:04 -0500

    avance en casita

--- short-link-note/imports/ui/Signup.js
+++ short-link-note/imports/ui/Signup.js
@@ -2,12 +2,34 @@ import React from "react";
 import { Link } from "react-router";
 
 export default class Signup extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: '',
+       };
+  }
+  onSubmit(event){
+    event.preventDefault();
+    this.setState({
+      error: "Something went wrong."
+    })
+  }
   render() {
     return (
       <div>
         <h1>Singup component here</h1>
+
+        {this.state.error ? <p>{this.state.error}</p>: undefined}
+        
+        
         <Link to="/">Already have an acount? </Link>
-      </div>
+      <form onSubmit={this.onSubmit.bind(this)}>
+      <input type="email" name="email" placeholder="Email" required="true"/>
+      <input type="password" name="password" placeholder="Password" required="true"/>
+      <button> Create Account</button>
+      
+      </form>
+        </div>
     );
-  }
-}
+  
+}}
